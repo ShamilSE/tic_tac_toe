21dbb6959ad34820c9679ec9ed911dacb512ede8
Shamil <ishamil86@gmail.com>
2020-10-15 22:02:36 +0500

    removed win checker in another file

---
+++ is_win_checker.py
@@ -0,0 +1,18 @@
+def is_win(matrix):
+    row, col = 0, 0
+    while row < 3:
+        if matrix[row][col] == matrix[row][col + 1] == matrix[row][col + 2] != 0:
+            return True
+        row += 1
+    row, col = 0, 0
+    while col < 3:
+        if matrix[row][col] == matrix[row + 1][col] == matrix[row + 2][col] != 0:
+            return True
+        col += 1
+    row, col = 0, 0
+    if matrix[row][col] == matrix[row + 1][col + 1] == matrix[row + 2][col + 2] != 0:
+        return True
+    row, col = 0, 0
+    if matrix[row][col + 2] == matrix[row + 1][col + 1] == matrix[row + 2][col] != 0:
+        return True
+    return False
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from is_win_checker import is_win
 
 matrix = [
     ['', '', ''],
@@ -7,26 +8,6 @@ matrix = [
 ]
 
 print(matrix)
-
-
-def is_win():
-    row, col = 0, 0
-    while row < 3:
-        if matrix[row][col] == matrix[row][col + 1] == matrix[row][col + 2] != 0:
-            return True
-        row += 1
-    row, col = 0, 0
-    while col < 3:
-        if matrix[row][col] == matrix[row + 1][col] == matrix[row + 2][col] != 0:
-            return True
-        col += 1
-    row, col = 0, 0
-    if matrix[row][col] == matrix[row + 1][col + 1] == matrix[row + 2][col + 2] != 0:
-        return True
-    row, col = 0, 0
-    if matrix[row][col + 2] == matrix[row + 1][col + 1] == matrix[row + 2][col] != 0:
-        return True
-    return False
 
 
 def gen_numbers():
@@ -51,8 +32,9 @@ def player_step():
 
 
 def game():
-    while is_win():
+    while is_win(matrix):
         player_step()
+        print('win') if is_win(matrix) else print('play')
         print(matrix)
         comp_step()
         print(matrix)
